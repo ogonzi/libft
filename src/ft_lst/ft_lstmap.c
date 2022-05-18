212c4221244d07a726abda3ca4a2071bb93969da
Oscar Gonzalez simon <ogonzale@cbr5s5.42barcelona.com>
2022-05-18 12:09:23 +0200

    Pass --strict ft_lstmap

--- src/ft_lst/ft_lstmap.c
+++ src/ft_lst/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 13:20:36 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/05/17 16:29:55 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/05/18 12:06:04 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*f_lst;
 	t_list	*elem;
+	t_list	*temp;
 
 	if (!lst)
 		return (0);
 	f_lst = 0;
 	while (lst)
 	{
-		elem = ft_lstnew(f(lst->content));
+		temp = f(lst->content);
+		elem = ft_lstnew(temp);
 		if (!elem)
 		{
 			ft_lstclear(&f_lst, del);
+			del(temp);
 			return (0);
 		}
 		ft_lstadd_back(&f_lst, elem);
